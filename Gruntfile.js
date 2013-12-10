68473f65b9265a46c0dcbcf3578fd6c11b1b2a1c
cauliturtle <cauliturtle@gmail.com>
2013-12-10 16:28:31 +0800

    add grunt config and build setting

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,44 @@ module.exports = function(grunt) {
 		jshint: {
 			// define the files to lint
 			files: ['./src/*.js'],
+		},
+		less: {
+			compile: {
+				options: {
+					strictMath: true
+				},
+				files: {
+					'dist/css/<%= pkg.name %>.css': 'less/<%= pkg.name %>.less'
+				}
+			},
+			minify: {
+				options: {
+					cleancss: true,
+					report: 'min'
+				},
+				files: {
+					'dist/css/<%= pkg.name %>.min.css': 'dist/css/<%= pkg.name %>.css'
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '<%= banner %>',
+				report: 'min'
+			},
+			bootstrap: {
+				src: ['src/<%= pkg.name %>.js'],
+				dest: 'dist/js/<%= pkg.name %>.min.js'
+			}
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Default task(s).
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'less', 'uglify']);
 
 };
